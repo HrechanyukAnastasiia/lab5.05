a6136be170f592e223db20d8681ca7a95586ca22
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-03 21:38:06 +0300

    4

---
+++ lab5.05.py
@@ -0,0 +1,9 @@
+#4
+def long_words(dictionary):
+    long_words_list = []
+    for word in dictionary:
+        if len(word) >= 5:
+            long_words_list.append(word)
+    return long_words_list
+dict1 = {'dog', 'cat', 'tee', 'phone', 'computer', 'turtle'}
+print(long_words(dict1))
