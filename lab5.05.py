42c0eac2f10e174133628b8fde4558ca51cc14e4
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-03 21:33:50 +0300

    2

---
+++ lab5.05.py
@@ -0,0 +1,10 @@
+#2
+def count_vowels(s):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+s1 = input('Введіть рядок англ.мовою:')
+print('Кількість голосних літерів:', count_vowels(s1))
